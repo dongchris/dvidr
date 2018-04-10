36aa3c00c0a301841e7871ee1bf62c005c3fee97
dongchris <cadong@usfca.edu>
2018-04-10 15:52:11 -0700

    fix

--- websites/server.py
+++ websites/server.py
@@ -2,22 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/dvidr")
+@app.route("/")
 def homepage():
-    """Show a list of article titles"""
+    """Show homepage"""
     return render_template('homepage.html')
 
 @app.route("/dvidr/login")
 def user_login():
-    """Show a list of article titles"""
+    """Login / Register page -- not yet implemented"""
     return render_template('login.html')
-
-@app.route("/dvidr/upload")
-def upload_image():
-    """Show a list of article titles"""
-    return render_template('upload.html')
 
 if __name__ == '__main__':
     app.debug = True
     app.run()
-#app.run(host='localhost', port=80)
+# app.run(host='localhost', port=80)

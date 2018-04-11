0c53f5d10b8f5eb40afae36bb73ec0e9d8859021
dongchris <cadong@usfca.edu>
2018-04-10 19:34:24 -0700

    add python file

---
+++ code/readimage.py
@@ -0,0 +1,29 @@
+import boto
+from boto.s3.key import Key
+
+def readImagefromS3(imageFile):
+    """
+        This function will be using the pem file for AWS. 
+        It will access a public S3 bucket called dvidr with
+        the specified image file. The output is a url link
+        for the image that will be passed onto the Google
+        Cloud Vision API for processing.
+        
+    """
+    with open('dvidr.pem') as f:
+        keys = f.read().split(',')
+
+    s3 = boto.connect_s3(
+        aws_access_key_id=keys[0],
+        aws_secret_access_key=keys[1]
+    )
+    bucket = s3.get_bucket("dvidr", validate=False)
+
+    k1 = Key(bucket)
+    k1.key = imageFile
+
+    url_name = k1.generate_url(259200)
+    
+    return url_name
+
+print(readImagefromS3("85c.jpg"))
--- docs/_sources/tutorial.rst.txt
+++
@@ -1,4 +0,0 @@
-My Code Tutorial
-================
-
-Simply run $python server.py in the code directory.

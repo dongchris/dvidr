4b076976a4a4b4284b1eb3101f7cff7b61ef0e07
Qian Li <QIAN@Qians-MacBook-Pro.local>
2018-04-12 22:53:28 -0700

    pep8 for ocr

--- code/main/ocr.py
+++ code/main/ocr.py
@@ -9,6 +9,7 @@ import re
 from google.cloud import vision
 from google.cloud.vision import types
 
+
 def detect_text_uri(uri):
     """Detects text in the file located in Google Cloud Storage or on the Web.
     """
@@ -27,22 +28,25 @@ def detect_text_uri(uri):
                     for vertex in text.bounding_poly.vertices])
 
         print('bounds: {}'.format(','.join(vertices)))
-    
+
     return texts
 
+
 def bounding_box(url, texts):
-	"""
-	add bounding box
-	"""
+    """
+    add bounding box
+    """
     img = io.imread(url)    # read image from url
     for text in texts[1:]:  # 0th bounding box is whole picture
-        vertices = [(vertex.x, vertex.y)
-                for vertex in text.bounding_poly.vertices]   # get coordinates
-        cv2.polylines(img, [np.array(vertices)], True, (0,255,0), 2)  # plot line
+        vertices = [(vertex.x, vertex.y) for vertex in
+                    text.bounding_poly.vertices]   # get coordinates
+        # plot line
+        cv2.polylines(img, [np.array(vertices)], True, (0, 255, 0), 2)
 
-    plt.figure(figsize=(10,10))
+    plt.figure(figsize=(10, 10))
     plt.imshow(img)
     plt.show()
+
 
 def simple_process(text):
     text_list = text.split('\n')
@@ -57,10 +61,11 @@ def simple_process(text):
         result[key] = "$" + str(text_list[(result[key] + num_items)])
     return result
 
+
 if __name__ == '__main__':
-	url = 'https://dvidr.s3.amazonaws.com/85c.jpg?Signature=TNQbX0ndlcmvo4d2g%2F3FXbPonwE%3D&Expires=1523662875&AWSAccessKeyId=AKIAJ4FD4JXF5A7AKGGA'
-	texts = detect_text_uri(url)
-	bounding_box(url, texts)
-	print(simple_process(texts))
-
-
+    url = 'https://dvidr.s3.amazonaws.com/85c.jpg?Signature=TNQbX0nd'\
+            'lcmvo4d2g%2F3FXbPonwE%3D&Expires=1523662875&AWSAccessKey'\
+            'Id=AKIAJ4FD4JXF5A7AKGGA'
+    texts = detect_text_uri(url)
+    bounding_box(url, texts)
+    print(simple_process(texts))
